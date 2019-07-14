9d0c80448a984214e45e480657188d92fd7cac72
Julian Berman <Julian@GrayVines.com>
2019-07-14 19:30:03 -0400

    Patch bump.

--- Formula/ardour.rb
+++ Formula/ardour.rb
@@ -4,7 +4,7 @@ class Ardour < Formula
   url "https://community.ardour.org/srctar"
   version "5.12"
   sha256 "7e2a679b9a7eca7c72ec2fb9839b3e7d9e0049d83a8f9a1682788b5206fbd526"
-  head "https://github.com/Ardour/ardour"
+  head "https://github.com/Ardour/ardour.git"
 
   depends_on "aubio"
   depends_on "boost"
@@ -32,7 +32,7 @@ class Ardour < Formula
   patch :DATA
 
   def install
-    system "./waf", "configure", "--prefix=#{prefix}", "--with-backends=coreaudio,jack"
+    system "./waf", "configure", "--prefix=#{prefix}", "--with-backends=jack"
     system "./waf", "install"
 
     cd "./tools/osx_packaging" do
@@ -55,82 +55,79 @@ class Ardour < Formula
 end
 
 __END__
---- a/wscript     2018-10-13 16:17:12.000000000 -0400
-+++ b/wscript       2018-10-13 16:17:32.000000000 -0400
-@@ -384,6 +384,10 @@
-             conf.env['build_host'] = 'el_capitan'
-         elif re.search ("^16[.]", version) != None:
+diff --git a/wscript b/wscript
+--- a/wscript
++++ b/wscript
+@@ -403,6 +403,8 @@ int main() { return 0; }''',
              conf.env['build_host'] = 'sierra'
-+        elif re.search ("^17[.]", version) != None:
-+            conf.env['build_host'] = 'high_sierra'
+         elif re.search ("^17[.]", version) != None:
+             conf.env['build_host'] = 'high_sierra'
 +        elif re.search ("^18[.]", version) != None:
 +            conf.env['build_host'] = 'mojave'
          else:
              conf.env['build_host'] = 'irrelevant'
  
-@@ -409,8 +413,12 @@
-                 conf.env['build_target'] = 'yosemite'
-             elif re.search ("^15[.]", version) != None:
+@@ -430,8 +432,10 @@ int main() { return 0; }''',
                  conf.env['build_target'] = 'el_capitan'
--            else:
-+            elif re.search ("^16[.]", version) != None:
+             elif re.search ("^16[.]", version) != None:
                  conf.env['build_target'] = 'sierra'
+-            else:
 +            elif re.search ("^17[.]", version) != None:
-+                conf.env['build_target'] = 'high_sierra'
+                 conf.env['build_target'] = 'high_sierra'
 +            else:
 +                conf.env['build_target'] = 'mojave'
          else:
              match = re.search(
                      "(?P<cpu>i[0-6]86|x86_64|powerpc|ppc|ppc64|arm|s390x?)",
-@@ -431,11 +439,11 @@
+@@ -452,11 +456,11 @@ int main() { return 0; }''',
          #
          compiler_flags.append ('-U__STRICT_ANSI__')
  
--    if opt.use_libcpp or conf.env['build_host'] in [ 'el_capitan', 'sierra' ]:
+-    if opt.use_libcpp or conf.env['build_host'] in [ 'el_capitan', 'sierra', 'high_sierra' ]:
 +    if opt.use_libcpp or conf.env['build_host'] in [ 'el_capitan', 'sierra', 'high_sierra', 'mojave' ]:
         cxx_flags.append('--stdlib=libc++')
         linker_flags.append('--stdlib=libc++')
  
--    if conf.options.cxx11 or conf.env['build_host'] in [ 'mavericks', 'yosemite', 'el_capitan', 'sierra' ]:
+-    if conf.options.cxx11 or conf.env['build_host'] in [ 'mavericks', 'yosemite', 'el_capitan', 'sierra', 'high_sierra' ]:
 +    if conf.options.cxx11 or conf.env['build_host'] in [ 'mavericks', 'yosemite', 'el_capitan', 'sierra', 'high_sierra', 'mojave' ]:
          conf.check_cxx(cxxflags=["-std=c++11"])
          cxx_flags.append('-std=c++11')
          if platform == "darwin":
-@@ -443,7 +451,7 @@
+@@ -464,7 +468,7 @@ int main() { return 0; }''',
              # from requiring a full path to requiring just the header name.
              cxx_flags.append('-DCARBON_FLAT_HEADERS')
  
--            if not opt.use_libcpp and not conf.env['build_host'] in [ 'el_capitan', 'sierra' ]:
+-            if not opt.use_libcpp and not conf.env['build_host'] in [ 'el_capitan', 'sierra', 'high_sierra' ]:
 +            if not opt.use_libcpp and not conf.env['build_host'] in [ 'el_capitan', 'sierra', 'high_sierra', 'mojave' ]:
                  cxx_flags.append('--stdlib=libstdc++')
                  linker_flags.append('--stdlib=libstdc++')
              # Prevents visibility issues in standard headers
-@@ -452,7 +460,7 @@
+@@ -473,7 +477,7 @@ int main() { return 0; }''',
              cxx_flags.append('-DBOOST_NO_AUTO_PTR')
  
  
--    if (is_clang and platform == "darwin") or conf.env['build_host'] in [ 'mavericks', 'yosemite', 'el_capitan', 'sierra' ]:
+-    if (is_clang and platform == "darwin") or conf.env['build_host'] in [ 'mavericks', 'yosemite', 'el_capitan', 'sierra', 'high_sierra' ]:
 +    if (is_clang and platform == "darwin") or conf.env['build_host'] in [ 'mavericks', 'yosemite', 'el_capitan', 'sierra', 'high_sierra', 'mojave' ]:
          # Silence warnings about the non-existing osx clang compiler flags
          # -compatibility_version and -current_version.  These are Waf
          # generated and not needed with clang
-@@ -568,7 +576,7 @@
+@@ -589,7 +593,7 @@ int main() { return 0; }''',
                  ("-DMAC_OS_X_VERSION_MAX_ALLOWED=1090",
                   "-mmacosx-version-min=10.8"))
  
--    elif conf.env['build_target'] in ['el_capitan', 'sierra' ]:
+-    elif conf.env['build_target'] in ['el_capitan', 'sierra', 'high_sierra' ]:
 +    elif conf.env['build_target'] in ['el_capitan', 'sierra', 'high_sierra', 'mojave' ]:
          compiler_flags.extend(
                  ("-DMAC_OS_X_VERSION_MAX_ALLOWED=1090",
                   "-mmacosx-version-min=10.9"))
---- a/libs/backends/coreaudio/wscript     2018-10-13 16:38:33.000000000 -0400
-+++ b/libs/backends/coreaudio/wscript      2018-10-13 16:37:55.000000000 -0400
-@@ -31,7 +31,7 @@
-     obj.use      = 'libardour libpbd'
-     obj.uselib   = 'GLIBMM XML'
-     obj.framework = [ 'CoreAudio', 'AudioToolbox', 'CoreServices' ]
--    if bld.env['build_target'] not in [ 'lion', 'el_capitan' ] and (not bld.env['build_arch'] == "ppc"):
-+    if bld.env['build_target'] not in [ 'lion', 'el_capitan', 'sierra', 'high_sierra', 'mojave' ] and (not bld.env['build_arch'] == "ppc"):
-         obj.framework += [ 'CoreMidi' ]
-     else:
-         obj.framework += [ 'CoreMIDI' ]
+@@ -965,7 +969,6 @@ def configure(conf):
+         #       off processor type.  Need to add in a check
+         #       for that.
+         #
+-        conf.env.append_value('CXXFLAGS_OSX', '-F/System/Library/Frameworks')
+         conf.env.append_value('CXXFLAGS_OSX', '-F/Library/Frameworks')
+ 
+         conf.env.append_value('LINKFLAGS_OSX', ['-framework', 'AppKit'])
+-- 
+2.22.0
+
