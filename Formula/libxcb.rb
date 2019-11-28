89409ffe6923e2ffbddc78684c6e53504fbef944
Julian Berman <Julian@GrayVines.com>
2019-11-28 09:41:07 -0500

    More XCB things.

---
+++ Formula/libxcb.rb
@@ -0,0 +1,18 @@
+class Libxcb < Formula
+  desc "Interface to the X Window System protocol and replacement for Xlib"
+  homepage "https://www.x.org/"
+  url "https://xcb.freedesktop.org/dist/libxcb-1.13.1.tar.bz2"
+  sha256 "a89fb7af7a11f43d2ce84a844a4b38df688c092bf4b67683aef179cdf2a647c4"
+
+  depends_on "pkg-config" => :build
+  depends_on "xcb-proto" => :build
+  depends_on :x11
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
---
+++ Formula/xcb-proto.rb
@@ -0,0 +1,17 @@
+class XcbProto < Formula
+  desc "XML-XCB protocol descriptions that libxcb uses for code generation"
+  homepage "https://www.x.org/"
+  url "https://xcb.freedesktop.org/dist/xcb-proto-1.13.tar.bz2"
+  sha256 "7b98721e669be80284e9bbfeab02d2d0d54cd11172b72271e47a2fe875e2bde1"
+
+  depends_on "pkg-config" => :build
+  depends_on :x11
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
