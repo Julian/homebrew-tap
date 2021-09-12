332b6a08cc7d6cdcf0ee7156a67f996908824beb
Julian Berman <Julian@GrayVines.com>
2021-09-11 22:32:44 -0400

    Add a lean 4 formula.

---
+++ Formula/lean.rb
@@ -0,0 +1,37 @@
+class Lean < Formula
+  desc "Theorem Prover"
+  homepage "https://leanprover.github.io/"
+  url "https://github.com/leanprover/lean4/archive/v4.0.0-m2.tar.gz"
+  sha256 "e550b018a1fe8bbf18fbc275a332ce3ad436565bd6465aa0c96976fd3ae32a02"
+  license "Apache-2.0"
+  head "https://github.com/leanprover/lean4.git"
+
+  depends_on "cmake" => :build
+  depends_on "coreutils"
+  depends_on "gmp"
+  depends_on "jemalloc"
+  depends_on macos: :mojave
+
+  conflicts_with "elan-init", because: "`lean` and `elan-init` install the same binaries"
+
+  def install
+    mkdir "build/release" do
+      system "cmake", "../..", *std_cmake_args
+      system "make", "-j#{ENV.make_jobs}", "install"
+    end
+  end
+
+  test do
+    (testpath/"hello.lean").write <<~EOS
+      def id' {α : Type} (x : α) : α := x
+
+      example (a b : Prop) : a ∧ b -> b ∧ a :=
+      by
+        intro h
+        cases h
+        exact ⟨by assumption, by assumption⟩
+    EOS
+    system bin/"lean", testpath/"hello.lean"
+    system bin/"leanpkg", "help"
+  end
+end
