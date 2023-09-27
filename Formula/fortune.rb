b119c09c46fe393c684864793fa9c39c767a5a6f
Julian Berman <Julian@GrayVines.com>
2023-09-27 15:09:54 -0400

    Pull in Homebrew/homebrew-core#131548 to fix Fortune.
    Closes: #4

--- Formula/fortune.rb
+++ Formula/fortune.rb
@@ -4,6 +4,7 @@ class Fortune < Formula
   url "https://www.ibiblio.org/pub/linux/games/amusements/fortune/fortune-mod-9708.tar.gz"
   mirror "https://src.fedoraproject.org/repo/pkgs/fortune-mod/fortune-mod-9708.tar.gz/81a87a44f9d94b0809dfc2b7b140a379/fortune-mod-9708.tar.gz"
   sha256 "1a98a6fd42ef23c8aec9e4a368afb40b6b0ddfb67b5b383ad82a7b78d8e0602a"
+  license "BSD-4-Clause-UC"
 
   livecheck do
     url "https://www.ibiblio.org/pub/linux/games/amusements/fortune/"
@@ -34,3 +35,25 @@ class Fortune < Formula
     system "#{bin}/fortune"
   end
 end
+
+__END__
+--- a/util/rot.c
++++ b/util/rot.c
+@@ -5,6 +5,7 @@
+ 
+ #include <stdio.h>
+ #include <ctype.h>
++#include <stdlib.h>
+ 
+ int main(void)
+ {
+--- a/util/unstr.c
++++ b/util/unstr.c
+@@ -96,6 +96,7 @@ static char sccsid[] = "@(#)unstr.c	8.1 (Berkeley) 5/31/93";
+ #include	<ctype.h>
+ #include	<string.h>
+ #include	<unistd.h>
++#include	<stdlib.h>
+ 
+ #ifndef MAXPATHLEN
+ #define	MAXPATHLEN	1024
