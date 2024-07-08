5c4dd8991d59fbf85adcc360f2b274f68fbd1c39
Julian Berman <Julian@GrayVines.com>
2024-07-08 10:38:07 -0400

    Update the 0.9 neovim formula.

--- Formula/neovim@0.9.rb
+++ Formula/neovim@0.9.rb
@@ -4,16 +4,16 @@ class NeovimAT09 < Formula
   license "Apache-2.0"
 
   stable do
-    url "https://github.com/neovim/neovim/archive/v0.9.2.tar.gz"
-    sha256 "06b8518bad4237a28a67a4fbc16ec32581f35f216b27f4c98347acee7f5fb369"
+    url "https://github.com/neovim/neovim/archive/refs/tags/v0.9.5.tar.gz"
+    sha256 "fe74369fc30a32ec7a086b1013acd0eacd674e7570eb1acc520a66180c9e9719"
 
     # Remove when `mpack` resource is removed.
     depends_on "luarocks" => :build
 
     # Remove in 0.10.
     resource "mpack" do
-      url "https://github.com/libmpack/libmpack-lua/releases/download/1.0.10/libmpack-lua-1.0.10.tar.gz"
-      sha256 "18e202473c9a255f1d2261b019874522a4f1c6b6f989f80da93d7335933e8119"
+      url "https://github.com/libmpack/libmpack-lua/releases/download/1.0.11/libmpack-lua-1.0.11.tar.gz"
+      sha256 "a2d9ec184867ab92ad86e251908619fa13e345b8f2c9bc99df4ac63c8039d796"
     end
 
     # Keep resources updated according to:
@@ -22,27 +22,27 @@ class NeovimAT09 < Formula
     # TODO: Consider shipping these as separate formulae instead. See discussion at
     #       https://github.com/orgs/Homebrew/discussions/3611
     resource "tree-sitter-c" do
-      url "https://github.com/tree-sitter/tree-sitter-c/archive/v0.20.5.tar.gz"
+      url "https://github.com/tree-sitter/tree-sitter-c/archive/refs/tags/v0.20.5.tar.gz"
       sha256 "694a5408246ee45d535df9df025febecdb50bee764df64a94346b9805a5f349b"
     end
 
     resource "tree-sitter-lua" do
-      url "https://github.com/MunifTanjim/tree-sitter-lua/archive/v0.0.18.tar.gz"
+      url "https://github.com/MunifTanjim/tree-sitter-lua/archive/refs/tags/v0.0.18.tar.gz"
       sha256 "659beef871a7fa1d9a02c23f5ebf55019aa3adce6d7f5441947781e128845256"
     end
 
     resource "tree-sitter-vim" do
-      url "https://github.com/neovim/tree-sitter-vim/archive/v0.3.0.tar.gz"
+      url "https://github.com/neovim/tree-sitter-vim/archive/refs/tags/v0.3.0.tar.gz"
       sha256 "403acec3efb7cdb18ff3d68640fc823502a4ffcdfbb71cec3f98aa786c21cbe2"
     end
 
     resource "tree-sitter-vimdoc" do
-      url "https://github.com/neovim/tree-sitter-vimdoc/archive/v2.0.0.tar.gz"
+      url "https://github.com/neovim/tree-sitter-vimdoc/archive/refs/tags/v2.0.0.tar.gz"
       sha256 "1ff8f4afd3a9599dd4c3ce87c155660b078c1229704d1a254433e33794b8f274"
     end
 
     resource "tree-sitter-query" do
-      url "https://github.com/nvim-treesitter/tree-sitter-query/archive/v0.1.0.tar.gz"
+      url "https://github.com/nvim-treesitter/tree-sitter-query/archive/refs/tags/v0.1.0.tar.gz"
       sha256 "e2b806f80e8bf1c4f4e5a96248393fe6622fc1fc6189d6896d269658f67f914c"
     end
   end
@@ -54,7 +54,6 @@ class NeovimAT09 < Formula
 
   depends_on "cmake" => :build
   depends_on "lpeg" => :build # needed at runtime in 0.10.0
-  depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "libtermkey"
   depends_on "libuv"
@@ -76,34 +75,36 @@ class NeovimAT09 < Formula
       r.stage(buildpath/"deps-build/build/src"/r.name)
     end
 
-    cd "deps-build/build/src" do
-      # TODO: Remove `mpack` build block in 0.10.0.
-      cd "mpack" do
-        luajit = Formula["luajit"]
-        lua_path = "--lua-dir=#{luajit.opt_prefix}"
-        deps_build = buildpath/"deps-build"
+    if build.stable?
+      cd "deps-build/build/src" do
+        # TODO: Remove `mpack` build block in 0.10.0.
+        cd "mpack" do
+          luajit = Formula["luajit"]
+          lua_path = "--lua-dir=#{luajit.opt_prefix}"
+          deps_build = buildpath/"deps-build"
 
-        # The path separator for `LUA_PATH` and `LUA_CPATH` is `;`.
-        ENV.prepend "LUA_PATH", deps_build/"share/lua/5.1/?.lua", ";"
-        ENV.prepend "LUA_CPATH", deps_build/"lib/lua/5.1/?.so", ";"
+          # The path separator for `LUA_PATH` and `LUA_CPATH` is `;`.
+          ENV.prepend "LUA_PATH", deps_build/"share/lua/5.1/?.lua", ";"
+          ENV.prepend "LUA_CPATH", deps_build/"lib/lua/5.1/?.so", ";"
 
-        rock = "mpack-1.0.10-0.rockspec"
-        output = Utils.safe_popen_read("luarocks", "unpack", lua_path, rock, "--tree=#{deps_build}")
-        unpack_dir = output.split("\n")[-2]
+          rock = "mpack-1.0.11-0.rockspec"
+          output = Utils.safe_popen_read("luarocks", "unpack", lua_path, rock, "--tree=#{deps_build}")
+          unpack_dir = output.split("\n")[-2]
 
-        cd unpack_dir do
-          system "luarocks", "make", lua_path, "--tree=#{deps_build}"
+          cd unpack_dir do
+            system "luarocks", "make", lua_path, "--tree=#{deps_build}"
+          end
         end
-      end
 
-      Dir["tree-sitter-*"].each do |ts_dir|
-        cd ts_dir do
-          cp buildpath/"cmake.deps/cmake/TreesitterParserCMakeLists.txt", "CMakeLists.txt"
+        Dir["tree-sitter-*"].each do |ts_dir|
+          cd ts_dir do
+            cp buildpath/"cmake.deps/cmake/TreesitterParserCMakeLists.txt", "CMakeLists.txt"
 
-          parser_name = ts_dir[/^tree-sitter-(\w+)$/, 1]
-          system "cmake", "-S", ".", "-B", "build", "-DPARSERLANG=#{parser_name}", *std_cmake_args
-          system "cmake", "--build", "build"
-          system "cmake", "--install", "build"
+            parser_name = ts_dir[/^tree-sitter-(\w+)$/, 1]
+            system "cmake", "-S", ".", "-B", "build", "-DPARSERLANG=#{parser_name}", *std_cmake_args
+            system "cmake", "--build", "build"
+            system "cmake", "--install", "build"
+          end
         end
       end
     end
@@ -127,7 +128,7 @@ class NeovimAT09 < Formula
     ENV.append "LUA_CPATH", ";", ";"
 
     system "cmake", "-S", ".", "-B", "build",
-                    "-DLIBLUV_LIBRARY=#{Formula["luv"].opt_lib/shared_library("libluv")}",
+                    "-DLUV_LIBRARY=#{Formula["luv"].opt_lib/shared_library("libluv")}",
                     "-DLIBUV_LIBRARY=#{Formula["libuv"].opt_lib/shared_library("libuv")}",
                     "-DLPEG_LIBRARY=#{Formula["lpeg"].opt_lib/shared_library("liblpeg")}",
                     *std_cmake_args
